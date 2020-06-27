e2675a07c512a6fed37564def880dcbf8ad2e4bd
Tony G <tlguzman94@gmail.com>
2020-06-27 14:47:21 -0400

    modified the shoe detail view/controller

--- controllers/shoe.js
+++ controllers/shoe.js
@@ -14,28 +14,31 @@ const shoe_list = (req, res, next) => {
 };
 
 const shoe_detail = (req, res, next) => {
-  Shoe.findById(req.params.id, (err, shoe) => {
-    if (err) {
-      next(err);
-      return;
-    }
-    if (!shoe) {
-      const err = new Error('Shoe not found');
-      err.status = 404;
-      next(err);
-      return;
-    }
-    ShoeInstance.find({ shoe: shoe.id }, (err, shoeInstances) => {
+  Shoe.findById(req.params.id)
+    .populate('brand category')
+    .exec((err, shoe) => {
       if (err) {
         next(err);
         return;
       }
-      res.render('shoe_detail', {
-        title: shoe.name,
-        shoeInstances,
+      if (!shoe) {
+        const err = new Error('Shoe not found');
+        err.status = 404;
+        next(err);
+        return;
+      }
+      ShoeInstance.find({ shoe: shoe.id }, (err, shoeInstances) => {
+        if (err) {
+          next(err);
+          return;
+        }
+        res.render('shoe_detail', {
+          title: 'Shoe Information',
+          shoe,
+          shoeInstances,
+        });
       });
     });
-  });
 };
 
 const shoe_instance_detail = (req, res, next) => {
