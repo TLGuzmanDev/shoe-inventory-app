6fb9cc3006f7de6b4f7d940e26a06c8cfe844bda
Tony G <tlguzman94@gmail.com>
2020-06-28 14:23:10 -0400

    added create shoe view and controller

--- controllers/shoe.js
+++ controllers/shoe.js
@@ -1,5 +1,9 @@
+const async = require('async');
+const validator = require('express-validator');
 const Shoe = require('../models/shoe');
 const ShoeInstance = require('../models/shoeInstance');
+const Brand = require('../models/brand');
+const Category = require('../models/category');
 
 const shoe_list = (req, res, next) => {
   Shoe.find({})
@@ -41,7 +45,98 @@ const shoe_detail = (req, res, next) => {
     });
 };
 
+const shoe_create_get = (req, res, next) => {
+  async.parallel(
+    {
+      brand_list: (callback) => {
+        Brand.find({}, callback);
+      },
+      category_list: (callback) => {
+        Category.find({}, callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.render('shoe_form', {
+        title: 'Create Shoe',
+        brands: results.brand_list,
+        categories: results.category_list,
+      });
+    }
+  );
+};
+
+const shoe_create_post = [
+  validator.body('name', 'Invalid shoe name').trim().isLength({ min: 3 }),
+
+  validator.body('brand', 'Invalid shoe brand').trim().isLength({ min: 3 }),
+
+  validator
+    .body('category', 'Invalid shoe category')
+    .trim()
+    .isLength({ min: 3 }),
+
+  validator
+    .body('description', 'Invalid shoe description')
+    .optional({ checkFalsy: true })
+    .trim()
+    .isLength({ min: 3 }),
+
+  validator.body('*').escape(),
+
+  (req, res, next) => {
+    const errors = validator.validationResult(req);
+
+    const shoe = new Shoe({
+      name: req.body.name,
+      brand: req.body.brand,
+      category: req.body.category,
+      description: req.body.description,
+      price: req.body.price,
+    });
+
+    if (!errors.isEmpty()) {
+      async.parallel(
+        {
+          brand_list: (callback) => {
+            Brand.find({}, callback);
+          },
+          category_list: (callback) => {
+            Category.find({}, callback);
+          },
+        },
+        (err, results) => {
+          if (err) {
+            next(err);
+            return;
+          }
+          res.render('shoe_form', {
+            title: 'Create Shoe',
+            brands: results.brand_list,
+            categories: results.category_list,
+            errors: errors.array(),
+          });
+        }
+      );
+      return;
+    } else {
+      shoe.save((err) => {
+        if (err) {
+          next(err);
+          return;
+        }
+        res.redirect(shoe.url);
+      });
+    }
+  },
+];
+
 module.exports = {
   shoe_list,
   shoe_detail,
+  shoe_create_get,
+  shoe_create_post,
 };
--- routes/shoes.js
+++ routes/shoes.js
@@ -7,6 +7,10 @@ router.get('/', (req, res, next) => {
   shoeController.shoe_list(req, res, next);
 });
 
+router.get('/shoe/create', shoeController.shoe_create_get);
+
+router.post('/shoe/create', shoeController.shoe_create_post);
+
 router.get('/shoe/:id', (req, res, next) => {
   shoeController.shoe_detail(req, res, next);
 });
