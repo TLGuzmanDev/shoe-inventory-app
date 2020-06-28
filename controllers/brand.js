5daecbe0f3fee461b31e034882c284a89062e0f8
Tony G <tlguzman94@gmail.com>
2020-06-28 01:31:50 -0400

    created the controllers and views for creating new brands

--- controllers/brand.js
+++ controllers/brand.js
@@ -1,3 +1,4 @@
+const validator = require('express-validator');
 const Brand = require('../models/brand');
 const Shoe = require('../models/shoe');
 
@@ -41,7 +42,55 @@ const brand_detail = (req, res, next) => {
   });
 };
 
+const brand_create_get = (req, res, next) => {
+  res.render('brand_form', {
+    title: 'Create Brand',
+  });
+};
+
+const brand_create_post = [
+  validator.body('name', 'Invalid brand name').trim().isLength({ min: 1 }),
+  validator.body('founded', 'Invalid founded date').trim().isDate(),
+  validator.body('name').escape(),
+  validator.body('founded').escape(),
+  (req, res, next) => {
+    const errors = validator.validationResult(req);
+    const brand = new Brand({
+      name: req.body.name,
+      founded: req.body.founded,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('brand_form', {
+        title: 'Create Brand',
+        errors: errors.array(),
+        brand,
+      });
+    } else {
+      // Data from form is valid
+      Brand.findOne({ name: req.body.name }).exec((err, found_brand) => {
+        if (err) {
+          next(err);
+          return;
+        }
+        if (found_brand) {
+          res.redirect(found_brand.url);
+        } else {
+          brand.save((err) => {
+            if (err) {
+              next(err);
+              return;
+            }
+            res.redirect(brand.url);
+          });
+        }
+      });
+    }
+  },
+];
 module.exports = {
   brand_list,
   brand_detail,
+  brand_create_get,
+  brand_create_post,
 };
--- routes/brands.js
+++ routes/brands.js
@@ -3,12 +3,28 @@ const router = express.Router();
 
 const brandController = require('../controllers/brand');
 
-router.get('/', (req, res, next) => {
-  brandController.brand_list(req, res, next);
+router.get('/brand/create', brandController.brand_create_get);
+
+router.post('/brand/create', brandController.brand_create_post);
+
+router.get('/brand/:id/update', (req, res, next) => {
+  res.send('not implemented');
 });
 
-router.get('/:id', (req, res, next) => {
-  brandController.brand_detail(req, res, next);
+router.post('/brand/:id/update', (req, res, next) => {
+  res.send('not implemented');
 });
+
+router.get('/brand/:id/delete', (req, res, next) => {
+  res.send('not implemented');
+});
+
+router.post('/brand/:id/delete', (req, res, next) => {
+  res.send('not implemented');
+});
+
+router.get('/brand/:id', brandController.brand_detail);
+
+router.get('/', brandController.brand_list);
 
 module.exports = router;
